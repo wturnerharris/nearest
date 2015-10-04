b5518f76bebaf7655b33593c0c5707a8d3e2c937
Wes Turner <wturnerharris@gmail.com>
2015-10-04 11:45:47 -0400

    Add application logic
    - Fix retrieval of json data
    - Add detectors for location and internet

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,23 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
+using Android.Views.Animations;
 using Android.Widget;
 using Android.OS;
 using Android.Text;
 using Android.Graphics;
-using Android.Views.Animations;
+using Android.Locations;
+//using Android.Database.Sqlite;
+using Android.Net;
 
 using Nearest.ViewModels;
+using Nearest.Models;
 
 namespace Nearest.Droid
 {
-	[Activity (Label = "Nearest.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
+	[Activity (Label = "Nearest", 
+		MainLauncher = true, 
+		Icon = "@drawable/icon", 
+		ScreenOrientation = ScreenOrientation.Portrait
+	)]
+	public class MainActivity : Activity, ILocationListener
 	{
 		int count = 1;
+		LocationManager locationMgr;
+		public TrainListViewModel trainLVM;
+
+		/**
+		 * Public UI Elements
+		 * 
+		 */
+		public LinearLayout uptown;
+		public LinearLayout downtown;
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -29,7 +49,33 @@ namespace Nearest.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			//TrainListViewModel train = new TrainListViewModel ();
+			/**
+			 * Detect internet access
+			 * 
+			 */
+			ConnectivityManager connectivityManager = (ConnectivityManager) GetSystemService(ConnectivityService);
+			NetworkInfo activeConnection = connectivityManager.ActiveNetworkInfo;
+			bool isConnected = (activeConnection != null) && activeConnection.IsConnected;
+			var locationProvider = checkLocation ();
+			if(locationProvider != null) {
+				//locationMgr.RequestLocationUpdates (locationProvider, 2000, 1, this);
+				if (isConnected) {
+					Location location = locationMgr.GetLastKnownLocation (locationProvider);
+					System.Console.WriteLine(
+						"DBG: Lat: " + location.Latitude + 
+						"\nDBG: Long: " + location.Longitude
+					);
+					trainLVM = new TrainListViewModel (location.Latitude, location.Longitude);
+					getTrainPaths();
+				} else {
+					// Create ui-based handler for no internet
+					System.Console.WriteLine ("DBG: No internet is available");
+				}
+			} else {
+				// Create ui-based handler for unknown location
+				System.Console.WriteLine ("DBG: No location providers available");
+			}
+
 
 			// Set Typeface and Styles
 			Typeface HnBd = Typeface.CreateFromAsset(Assets,"fonts/HelveticaNeueLTCom-Bd.ttf");
@@ -45,10 +91,14 @@ namespace Nearest.Droid
 			TextView timeN = FindViewById<TextView> (Resource.Id.TextTimeNorth);
 			TextView destN = FindViewById<TextView> (Resource.Id.TextDestNorth);
 
-			Animation hyperspaceJump = AnimationUtils.LoadAnimation(
+			downtown = FindViewById<LinearLayout> (Resource.Id.LayoutDowntown);
+			uptown = FindViewById<LinearLayout> (Resource.Id.LayoutUptown);
+
+
+			/*Animation hyperspaceJump = AnimationUtils.LoadAnimation(
 				this, 
 				Resource.Animation.tada
-			);
+			);*/
 
 			title.SetTypeface(HnBd, tfs);
 			tagLn.SetTypeface(HnLt, tfs);
@@ -57,20 +107,21 @@ namespace Nearest.Droid
 			timeN.SetTypeface(HnMd, tfs);
 			destN.SetTypeface(HnLt, tfs);
 
-			// Get our button from the layout resource,
-			Button buttonNorth = FindViewById<Button> (Resource.Id.ButtonNorth);
-
-			// and attach an event to it
+			/**
+			 * Define UI actions
+			 * 
+			 */
+			Button buttonNorth = uptown.FindViewById<Button> (Resource.Id.ButtonNorth);
 			buttonNorth.Click += delegate {
-				buttonNorth.Text = string.Format ("{0}", count++);
+				//buttonNorth.Text = string.Format ("{0}", count++);
 				buttonNorth.SetBackgroundResource(
 					count%2==0?Resource.Drawable.orange:Resource.Drawable.purple
 				);
 			};
 
-			Button buttonSouth = FindViewById<Button> (Resource.Id.ButtonSouth);
+			Button buttonSouth = downtown.FindViewById<Button> (Resource.Id.ButtonSouth);
 			buttonSouth.Click += delegate {
-				title.StartAnimation(hyperspaceJump);
+				//title.StartAnimation(hyperspaceJump);
 				buttonSouth.SetBackgroundResource(
 					count%2==0?Resource.Drawable.red:Resource.Drawable.gray
 				);
@@ -78,10 +129,170 @@ namespace Nearest.Droid
 
 		}
 
-		/** Called when the user touches a button */
+		/**
+		 * Resume Activity
+		 * 
+		 */
+		protected override void OnResume () {
+			base.OnResume();
+			checkLocation();
+		}
+
+		/**
+		 * Detect Location
+		 * 
+		 */
+		public string checkLocation(){
+			locationMgr = GetSystemService (Context.LocationService) as LocationManager;
+			Criteria locationCriteria = new Criteria();
+			locationCriteria.Accuracy = Accuracy.Fine;
+			locationCriteria.PowerRequirement = Power.NoRequirement;
+			String locationProviderName = locationMgr.GetBestProvider (locationCriteria, true);
+			String Provider = LocationManager.GpsProvider;
+
+			if(locationMgr.IsProviderEnabled(locationProviderName))
+			{
+				locationMgr.RequestLocationUpdates(locationProviderName, 30000, 1, this);
+			}
+			else
+			{
+				System.Console.WriteLine("DBG: "+ 
+					locationProviderName + " " + Provider + 
+					" is not available. Does the device have location services enabled?"
+				);
+			}
+			return locationProviderName;
+		}
+
+		public void setNextTrains() {
+			if ( trainLVM.IsBusy ) {
+				System.Console.WriteLine("DBG: Async busy");
+			} else {
+				//TODO: refactor to a loop 
+				var btnDown = downtown.FindViewById<Button> (Resource.Id.ButtonSouth);
+				var timeDown = downtown.FindViewById<TextView> (Resource.Id.TextTimeSouth);
+				if ( trainLVM.stopListDowntown.Count > 0 ) {
+					var nearestDown = trainLVM.stopListDowntown[0].next_train;
+					btnDown.Text = nearestDown.route_id;
+					btnDown.SetBackgroundResource( getTrainColor(nearestDown.route_id) );
+					timeDown.Text = Train.time (nearestDown.ts);
+					//btnDown.SetTextColor();
+				}
+				var btnUp = uptown.FindViewById<Button> (Resource.Id.ButtonNorth);
+				var timeUp = uptown.FindViewById<TextView> (Resource.Id.TextTimeNorth);
+				if ( trainLVM.stopListUptown.Count > 0 ) {
+					var nearestUp = trainLVM.stopListUptown[0].next_train;
+					btnUp.Text = nearestUp.route_id;
+					btnUp.SetBackgroundResource( getTrainColor(nearestUp.route_id) );
+					timeUp.Text = Train.time (nearestUp.ts);
+					//btnUp.SetTextColor();
+				}
+			}
+		}
+
+		/**
+		 * Get Train Color
+		 * 
+		 */
+		public int getTrainColor( String StopId ) {
+			int resourceDrawable;
+			switch (StopId) 
+			{
+				case "1":
+				case "2":
+				case "3":
+					resourceDrawable = Resource.Drawable.red;
+					break;
+				case "A":
+				case "C":
+				case "E":
+					resourceDrawable = Resource.Drawable.blue;
+					break;
+				case "N":
+				case "Q":
+				case "R":
+					resourceDrawable = Resource.Drawable.yellow;
+					break;
+				case "4":
+				case "5":
+				case "6":
+				case "G": /** TODO: This is alternate green */
+					resourceDrawable = Resource.Drawable.green;
+					break;
+				case "B":
+				case "D":
+				case "F":
+				case "M":
+					resourceDrawable = Resource.Drawable.orange;
+					break;
+				case "7":
+					resourceDrawable = Resource.Drawable.purple;
+					break;
+				case "J":
+				case "Z":
+					resourceDrawable = Resource.Drawable.brown;
+					break;
+				case "L":
+				default: 
+					resourceDrawable = Resource.Drawable.gray;
+					break;
+			}
+			return resourceDrawable;
+		}
+
+		/**
+		 * Get List of Trains for VM
+		 * 
+		 */
+		async void getTrainPaths() {
+			try 
+			{
+				await trainLVM.GetTrainsAsync();
+			}
+			catch ( Exception ex )
+			{
+				System.Console.WriteLine("DBG: Error: No Trains Found");
+				System.Console.WriteLine("DBG: " + ex.Message.ToString());
+			}
+			finally {
+				if (!this.trainLVM.IsBusy) {
+					System.Console.WriteLine("DBG: Async Complete");
+					setNextTrains();
+				}
+			}
+		}
+
+		/**
+		 * Called when the user touches a button 
+		 * 
+		 */
 		public void sendMessage(View view) {
 			// Do something in response to button click
-			System.Console.WriteLine("sendingMessage");
+			System.Console.WriteLine("DBG: sendingMessage");
+		}
+
+		/**
+		 * Location Updates
+		 * 
+		 */
+		public void OnProviderEnabled (string provider)
+		{
+			
+		}
+
+		public void OnProviderDisabled (string provider)
+		{
+			
+		}
+
+		public void OnStatusChanged (string provider, Availability status, Bundle extras)
+		{
+			
+		}
+
+		public void OnLocationChanged (Android.Locations.Location location)
+		{
+			
 		}
 	}
 }
--- Nearest/ViewModels/TrainListViewModel.cs
+++ Nearest/ViewModels/TrainListViewModel.cs
@@ -3,19 +3,26 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Net.Http;
 
 using Nearest.Models;
-using System.Net.Http;
 
 namespace Nearest.ViewModels
 {
 	public class TrainListViewModel : INotifyPropertyChanged
 	{
-		public ObservableCollection<Train> TrainList { get; set; }
+		String latitude;
+		String longitude;
+		public String requestString;
+		public ObservableCollection<Stop> stopListUptown { get; set; }
+		public ObservableCollection<Stop> stopListDowntown { get; set; }
 
-		public TrainListViewModel ()
+		public TrainListViewModel (Double lat, Double lon)
 		{
-			TrainList = new ObservableCollection<Train> ();
+			latitude = lat.ToString();
+			longitude = lon.ToString();
+			stopListUptown = new ObservableCollection<Stop> ();
+			stopListDowntown = new ObservableCollection<Stop> ();
 		}
 
 
@@ -40,14 +47,21 @@ namespace Nearest.ViewModels
 
 				var client = new HttpClient();
 
-				var json = await client.GetStringAsync("http://turnerharris.com/nearest/" +
-					"next.php?action=getTrains&lat=40.8248438&lon=-73.95145959999999&dir=0");
+				requestString = "http://turnerharris.com/nearest/" +
+					"next.php?action=getTrains&lat=" + latitude + "&lon=" + longitude;
 
-				var items = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Train>>(json);
-
-				foreach(var item in items)
+				for(int i = 0; i < 2; i++)
 				{
-					TrainList.Add(item);
+					var json = await client.GetStringAsync(requestString + "&dir="+ i.ToString());
+					var items = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Stop>>(json);
+
+					foreach( var item in items){
+						if (i==0) {
+							stopListDowntown.Add(item);
+						} else {
+							stopListUptown.Add(item);
+						}
+					}
 				}
 			}
 			finally 
