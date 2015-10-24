6f80b8ce2407ad5ca46c39639e532a3971c28f00
Wes Turner <wturnerharris@gmail.com>
2015-10-24 14:40:13 -0400

    Add alert view

---
+++ Droid/Alert.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Java.Lang;
+
+namespace Nearest.Droid
+{
+	[Activity (Label = "Alert")]			
+	public class Alert : Activity
+	{
+		static int EXIT_DELAY = 3500;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Create your application here
+			RequestWindowFeature (WindowFeatures.NoTitle);
+			SetContentView (Resource.Layout.Alert);
+			TextView details = (TextView)FindViewById (Resource.Id.AlertContent);
+			this.Window.SetBackgroundDrawableResource (Android.Resource.Color.Transparent);
+			string strNotificationToShow = "Oops.. Something went wrong.";
+			int myParentWidth = this.Window.Attributes.Width;
+			try {
+				Bundle extras = this.Intent.Extras;
+				if (extras != null) {	
+					strNotificationToShow = extras.Get ().toString ();
+					myParentWidth = extras.GetInt ();
+				} else {
+					strNotificationToShow = "Oops.. Something went wrong.";
+					myParentWidth = this.Window.Attributes.Width;
+				}
+			} catch (Exception e) {
+				//e.PrintStackTrace ();
+				strNotificationToShow = "Oops.. Something went wrong.";
+				myParentWidth = this.Window.Attributes.Width;
+			} finally {
+				details.Text = strNotificationToShow;
+			}
+			WindowManagerLayoutParams Params = this.Window.Attributes;
+			//WindowManager.LayoutParams param = this.Window.Attributes;  
+			Params.Width = myParentWidth;
+			Params.Gravity = GravityFlags.Bottom;
+
+			this.Window.Attributes = (Params);
+
+			Window window = this.Window;
+			window.Attributes = ((WindowManagerLayoutParams)Params);
+			window.ClearFlags (WindowManagerFlags.DimBehind);
+			window.SetFlags (WindowManagerFlags.NotTouchModal, WindowManagerFlags.NotTouchModal);
+
+			new Handler ().PostDelayed (Exit (), EXIT_DELAY);
+
+		}
+
+		private Runnable Exit ()
+		{
+			return new Runnable () {
+				Run = () => Finish ()
+			};
+		}
+	}
+}
