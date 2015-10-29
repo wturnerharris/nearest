ebd42cd2a113e40816cc88956163c38f287a1db3
Wes Turner <wturnerharris@gmail.com>
2015-10-28 21:10:20 -0400

    Modify detail activity payload details

--- Droid/Detail.cs
+++ Droid/Detail.cs
@@ -26,13 +26,13 @@ namespace Nearest.Droid
 			TextView RouteView = FindViewById<TextView> (Resource.Id.textView1);
 
 			var json = Intent.GetStringExtra ("trainLVM");
-			var dir = Intent.GetIntExtra ("direction", 0);
+			var dir = Intent.GetStringExtra ("direction");
 			if (json != null) {
 				var items = Newtonsoft.Json.JsonConvert.DeserializeObject<List<List<Stop>>> (json);
 				RouteView.Text = String.Format (
-					"Total Payload: {0}\nDirection: {1}\nDirection: {2}", 
+					"Total Payload: {0}\nDirection: {1}\nData: {2}", 
 					items.Count, 
-					dir > 0 ? "North" : "South", 
+					dir, 
 					json
 				);
 			} else {
