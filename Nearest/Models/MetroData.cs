d0ead9fa7360017430b9b1256d791b382f1fd49c
Wes Turner <wturnerharris@gmail.com>
2015-12-06 10:24:26 -0500

    Initial commit of core application logic

---
+++ Nearest/Models/MetroData.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using SQLite;
+
+namespace Nearest.Models
+{
+	public class MetroData
+	{
+
+		public MetroData ()
+		{
+			
+		}
+
+		/*
+		CREATE TABLE `calendar` (
+			`service_id` varchar(255) DEFAULT NULL,
+			`monday` int(11) DEFAULT NULL,
+			`tuesday` int(11) DEFAULT NULL,
+			`wednesday` int(11) DEFAULT NULL,
+			`thursday` int(11) DEFAULT NULL,
+			`friday` int(11) DEFAULT NULL,
+			`saturday` int(11) DEFAULT NULL,
+			`sunday` int(11) DEFAULT NULL,
+			`start_date` date DEFAULT NULL,
+			`end_date` date DEFAULT NULL,
+			KEY `service_id` (`service_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		*/
+		public class calendar
+		{
+			[PrimaryKey, AutoIncrement, Column ("service_id")]
+			public string ServiceId { get; set; }
+
+			[MaxLength (11), Column ("monday")]
+			public int Monday { get; set; }
+
+			[MaxLength (11), Column ("tuesday")]
+			public int Tuesday { get; set; }
+
+			[MaxLength (11), Column ("wednesday")]
+			public int Wednesday { get; set; }
+
+			[MaxLength (11), Column ("thursday")]
+			public int Thursday { get; set; }
+
+			[MaxLength (11), Column ("friday")]
+			public int Friday { get; set; }
+
+			[MaxLength (11), Column ("saturday")]
+			public int Saturday { get; set; }
+
+			[MaxLength (11), Column ("sunday")]
+			public int Sunday { get; set; }
+
+			[Column ("start_date")]
+			public DateTime StartDate { get; set; }
+
+			[Column ("end_date")]
+			public DateTime endDate { get; set; }
+		}
+
+		/*
+		CREATE TABLE `stop_times` (
+			`trip_id` varchar(255) DEFAULT NULL,
+			`arrival_time` time DEFAULT NULL,
+			`departure_time` time DEFAULT NULL,
+			`stop_id` varchar(255) DEFAULT NULL,
+			`stop_sequence` int(11) DEFAULT NULL,
+			`stop_headsign` varchar(255) DEFAULT NULL,
+			`pickup_type` int(11) DEFAULT NULL,
+			`drop_off_type` int(11) DEFAULT NULL,
+			`shape_dist_traveled` varchar(255) DEFAULT NULL,
+		KEY `stop_time_index` (`trip_id`,`arrival_time`,`departure_time`,`stop_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		*/
+		public class stop_times
+		{
+			public string trip_id{ get; set; }
+
+			public string stop_id{ get; set; }
+
+			public string stop_headsign{ get; set; }
+
+			public string shape_dist_traveled{ get; set; }
+
+			public int stop_sequence{ get; set; }
+
+			public int pickup_type{ get; set; }
+
+			public int drop_off_type{ get; set; }
+		}
+
+		/*
+		CREATE TABLE `stops` (
+			`stop_id` varchar(255) DEFAULT NULL,
+			`stop_code` varchar(255) DEFAULT NULL,
+			`stop_name` varchar(255) DEFAULT NULL,
+			`stop_desc` varchar(255) DEFAULT NULL,
+			`stop_lat` float(10,6) DEFAULT NULL,
+			`stop_lon` float(10,6) DEFAULT NULL,
+			`zone_id` varchar(255) DEFAULT NULL,
+			`stop_url` varchar(255) DEFAULT NULL,
+			`location_type` int(11) DEFAULT NULL,
+			`parent_station` varchar(255) DEFAULT NULL,
+		KEY `stop_id` (`stop_id`,`stop_name`,`stop_lat`,`stop_lon`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		*/
+		public class stops
+		{
+			public string stop_id{ get; set; }
+
+			public string stop_code{ get; set; }
+
+			public string stop_name{ get; set; }
+
+			public string stop_desc{ get; set; }
+
+			public string zone_id{ get; set; }
+
+			public string stop_url{ get; set; }
+
+			public string parent_station{ get; set; }
+
+			public int? location_type{ get; set; }
+		}
+
+		public class StopData
+		{
+			public string stop_id  { get; set; }
+
+			public double stop_lat  { get; set; }
+
+			public double stop_lon  { get; set; }
+
+			public string distance  { get; set; }
+		}
+
+		/*
+		CREATE TABLE `trips` (
+			`route_id` varchar(11) DEFAULT NULL,
+			`service_id` varchar(255) DEFAULT NULL,
+			`trip_id` varchar(255) DEFAULT NULL,
+			`trip_headsign` varchar(255) DEFAULT NULL,
+			`direction_id` int(11) DEFAULT NULL,
+			`block_id` varchar(255) DEFAULT NULL,
+			`shape_id` varchar(255) DEFAULT NULL,
+		KEY `route_id` (`route_id`,`service_id`,`trip_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+		*/
+		public class trips
+		{
+			public string route_id{ get; set; }
+
+			public string service_id{ get; set; }
+
+			public string trip_id{ get; set; }
+
+			public string trip_headsign{ get; set; }
+
+			public string block_id{ get; set; }
+
+			public string shape_id{ get; set; }
+
+			public int? direction_id{ get; set; }
+		}
+
+	}
+}
+
--- Nearest/Nearest.cs
+++ Nearest/Nearest.cs
@@ -1,13 +1,149 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Nearest.Models;
+using Newtonsoft.Json.Schema;
+
+using SQLite;
+using System.IO;
+
+//using ProtoBuf;
+//using transit_realtime;
+using System.Globalization;
 
 namespace Nearest
 {
 	public class Nearest
 	{
+		public string ServiceCalendar;
+		const string DB_NAME = "nearest.db3";
+		public float latitude, longitude;
+		public SQLiteConnection database;
 
-		public Nearest ()
+		public Nearest (string path)
 		{
-			
+			ServiceCalendar = GetServiceCalendar ();
+
+			string dbPath = Path.Combine (path, DB_NAME);
+			database = new SQLiteConnection (dbPath);
+			//check if tables exist
+			//#if tables not exist
+			//download zip
+			//http://web.mta.info/developers/data/nyct/subway/google_transit.zip
+			//decompress zip
+			//insert specified csv tables to sqlite db
+			//#if
+			/*
+			date_default_timezone_set('America/New_York');
+			$this->mysql_tz = $mysqli->query("SET time_zone = 'America/New_York'");
+
+			.NET
+			Install-Package GtfsRealtimeBindings
+
+			WebRequest req = HttpWebRequest.Create("URL OF YOUR GTFS-REALTIME SOURCE GOES HERE");
+			FeedMessage feed = Serializer.Deserialize<FeedMessage>(req.GetResponse().GetResponseStream());
+			foreach (FeedEntity entity in feed.entity) {}
+			*/
+
+		}
+
+		private List<Stop> GetNearestStops (int dir = 0, int limit = 10, string unit = "M")
+		{
+			var lat = latitude;
+			var lon = longitude;
+			var _dir = dir > 0 ? "N" : "S";
+			var distance_threshold = 0.6;
+			double factor;
+
+			switch (unit.ToUpper ()) {
+			case "K":
+				factor = 1.609344;
+				break;
+			case "N":
+				factor = 0.8684;
+				break;
+			default:
+				factor = 1;
+				break;
+			}
+			var sql = String.Format (@"
+				SELECT stop_id, stop_lat, stop_lon, 
+				(
+					DEGREES(
+						ACOS(
+							( SIN( {0} ) * SIN( RADIANS(stop_lat) ) ) + (
+								COS( {0} ) * COS( RADIANS(stop_lat) ) * cos( RADIANS({1} - stop_lon) )
+							)
+						)
+					) * 60 * 1.1515 * {2} )
+				) AS distance FROM stops 
+				WHERE location_type = 1 
+				ORDER BY distance ASC LIMIT 5;", lat, lon, factor);
+
+			var results = database.Query<MetroData.StopData> (sql);
+			var stops = new List<Stop> ();
+			foreach (MetroData.StopData stop in results) {
+				if (double.Parse (stop.distance) > distance_threshold)
+					continue;
+				var stop_id = stop.stop_id;
+				var times = GetTrainsByStopId (stop_id, _dir);
+				var i = 0;
+				foreach (Train time in times) {
+					DateTime arrival = Convert.ToDateTime (time.arrival_time);
+					DateTime origin = new DateTime (1970, 1, 1, 0, 0, 0, 0);
+					TimeSpan diff = arrival.ToUniversalTime () - origin;
+					times [i].ts = (int)diff.TotalSeconds;
+					i++;
+				}
+
+				Train next_train = times.GetRange (0, 1) [0];
+				times.RemoveAt (0);
+				stops.Add (new Stop {
+					stop_id = (string)stop_id,
+					distance = stop.distance,
+					next_train = next_train,
+					direction = _dir,
+					trains = times
+				});
+			}
+			return stops;
+		}
+
+		private List<Train> GetTrainsByStopId (string stop_id, string cardinality = "N", int limit = 7)
+		{
+			stop_id += cardinality;
+			var time_compare = "CURTIME() + INTERVAL {0} MINUTE";
+
+			var sql = String.Format (@"
+				SELECT stop_times.arrival_time, trips.route_id, trips.trip_headsign, stops.stop_name
+				FROM stop_times, trips, stops WHERE arrival_time >= {0}
+				AND arrival_time <= {1}
+				AND stop_times.stop_id = '{2}'
+				AND trips.service_id = ANY ( {3} )
+				AND stop_times.stop_id = stops.stop_id
+				AND stop_times.trip_id = trips.trip_id 
+				ORDER BY arrival_time ASC LIMIT {4};", 
+				          String.Format (time_compare, 3), 
+				          String.Format (time_compare, 25),
+				          stop_id, 
+				          ServiceCalendar, 
+				          limit
+			          );
+			var results = database.Query<Train> (sql);
+			return results;
+		}
+
+		private string GetServiceCalendar ()
+		{
+			var DayOfWeek = DateTime.Today.DayOfWeek;
+			return String.Format ("SELECT service_id FROM calendar WHERE {0} = 1", 
+				DayOfWeek);
+		}
+
+		public double ConvertToRadians (double angle)
+		{
+			return (Math.PI / 180) * angle;
 		}
 	}
 }
