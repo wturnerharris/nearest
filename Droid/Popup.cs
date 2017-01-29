c9c92573e1573153bc4d9ecf2bb118686a033440
Wes Turner <wturnerharris@gmail.com>
2017-01-29 17:49:21 -0500

    Add popup activity to launch preferences

---
+++ Droid/Popup.cs
@@ -0,0 +1,22 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Preferences;
+
+namespace Nearest.Droid
+{
+	[Activity(Label = "Popup", Theme = "@style/InfoTheme")]
+	public class Popup : Activity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Display the fragment as the main content.
+			SetContentView(Resource.Layout.Popup);
+			FragmentTransaction FragTx = FragmentManager.BeginTransaction();
+			FragTx.Add(Resource.Id.content_container, new SettingsFragment()).Commit();
+
+		}
+	}
+}
