18029c0b2c5540a19e7158078b6dfb89fd4a6507
Wes Turner <wturnerharris@gmail.com>
2017-02-16 19:54:32 -0500

    Upgrade Stop Model
    * Add shift method to reduce the trains list property by one and shift that value to the next_train property
    * Add stale boolean property

--- Nearest/Models/Stop.cs
+++ Nearest/Models/Stop.cs
@@ -6,6 +6,8 @@ namespace Nearest.Models
 	public class Stop
 	{
 
+		public bool stale = false;
+
 		public string stop_id { get; set; }
 
 		public string distance { get; set; }
@@ -17,6 +19,19 @@ namespace Nearest.Models
 		public string direction { get; set; }
 
 		public EventHandler clickHandler { get; set; }
+
+		public void shift()
+		{
+			if (trains.Count < TRASH_BEYOND)
+			{
+				stale = true;
+			}
+			else
+			{
+			}
+			next_train = trains.GetRange(0, 1)[0];
+			trains.RemoveAt(0);
+		}
 	}
 }
 
